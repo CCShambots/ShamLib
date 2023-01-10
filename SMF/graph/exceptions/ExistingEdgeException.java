42b3c270c2f88908adb601937ec07006ab9d398c
J-Barta <jacob.barta25@gmail.com>
2023-01-10 12:39:12 -0500

    Remove unused exceptions. However, we might want to use them in the future or something.

--- SMF/graph/exceptions/ExistingEdgeException.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.ShamLib.SMF.graph.exceptions;
-
-public class ExistingEdgeException extends Exception{
-
-    public ExistingEdgeException() {
-        super();
-    }
-}
--- SMF/graph/exceptions/UnfoundVertexException.java
+++
@@ -1,6 +0,0 @@
-package frc.robot.ShamLib.SMF.graph.exceptions;
-
-public class UnfoundVertexException extends Exception{
-    public UnfoundVertexException() {
-    }
-}
