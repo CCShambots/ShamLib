41279587dfe81ed94d89f9f9851e318d88a63e8b
breadspeed1 <aiden@voth.name>
2023-01-12 10:22:23 -0500

    reorganize heirarchy

--- testing/graph/DirectionalEnumGraphTesting.java
+++ testing/graph/DirectionalEnumGraphTesting.java
@@ -1,6 +1,5 @@
-package frc.robot.ShamLib.testing;
+package frc.robot.ShamLib.testing.graph;
 
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.ShamLib.SMF.graph.DirectionalEnumGraph;
 import frc.robot.ShamLib.SMF.graph.EdgeType;
