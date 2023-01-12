96cf54dafe10be1ad3989252de754eaebbe204ab
J-Barta <jacob.barta25@gmail.com>
2023-01-12 16:46:23 -0500

    Make limelight class not a singleton

--- vision/Limelight.java
+++ vision/Limelight.java
@@ -6,62 +6,66 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Limelight {
 
-        private static Limelight instance;
 
-        private NetworkTable table;
+    private NetworkTable table;
 
-        /**Limelight subsystem that returns all the information we use for targetting */
-        private Limelight() {
-            this.table = getLimeLightTable();
+    /**
+     * Creates a limelight object
+     * @param tableID the id of the network table where the limelight will post its data
+     */
+    public Limelight(String tableID) {
+            this.table = getLimeLightTable(tableID);
         }
 
-        public static Limelight getInstance() {
-            if(instance == null) instance = new Limelight();
-            return instance;
-        }
+    /**
+     * Creates a limelight object with the default network table name - "limelight"
+     */
+    public Limelight() {
+        this.table = getLimeLightTable("limelight");
+    }
 
-        /**
-         * @return The limelight's data table (used to access values from the pipeline)
-         */
-        private NetworkTable getLimeLightTable() {
-            return NetworkTableInstance.getDefault().getTable("limelight");
-        }
+    /**
+     * @return The limelight's data table (used to access values from the pipeline)
+     */
+    private NetworkTable getLimeLightTable(String tableID) {
+        return NetworkTableInstance.getDefault().getTable(tableID);
+    }
 
-        /**
-         * @return true if the limelight has identified a valid target
-         */
-        public boolean hasTarget() {
-            return table.getEntry("tv").getDouble(0) == 1;
-        }
+    /**
+     * @return true if the limelight has identified a valid target
+     */
+    public boolean hasTarget() {
+        return table.getEntry("tv").getDouble(0) == 1;
+    }
 
-        public Rotation2d getXOffset() {
-            return Rotation2d.fromDegrees(-getLimeLightTable().getEntry("tx").getDouble(0));
-        }
+    public Rotation2d getXOffset() {
+        return Rotation2d.fromDegrees(-table.getEntry("tx").getDouble(0));
+    }
 
-        public Rotation2d getYOffset() {
-            return Rotation2d.fromDegrees(getLimeLightTable().getEntry("ty").getDouble(0));
+    public Rotation2d getYOffset() {
+        return Rotation2d.fromDegrees(table.getEntry("ty").getDouble(0));
 
-        }
+    }
 
-        /**
-         * Turns on the limelight
-         */
-        public void setOn() {
-            getLimeLightTable().getEntry("ledMode").setNumber(3);
-        }
+    /**
+     * Turns on the limelight
+     */
+    public void setOn() {
+        table.getEntry("ledMode").setNumber(3);
+    }
 
-        /**
-         * Turns off the limelight
-         */
-        public void setOff() {
-            getLimeLightTable().getEntry("ledMode").setNumber(1);
-        }
+    /**
+     * Turns off the limelight
+     */
+    public void setOff() {
+        table.getEntry("ledMode").setNumber(1);
+    }
 
-        /**
-         * @return the latency of the limelight (in ms)
-         */
-        public double getLatency() {
-            return getLimeLightTable().getEntry("tl").getDouble(0);
-        }
+    /**
+     * @return the latency of the limelight (in ms)
+     */
+    public double getLatency() {
+        return table.getEntry("tl").getDouble(0);
+    }
 
 }
