0430801ad181b0a41d5ae9fe75fd0e7c893ec031
breadspeed1 <aiden@voth.name>
2023-01-16 13:10:51 -0500

    remove exceptions

--- SMF/exceptions/IncompatibleStateException.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.ShamLib.SMF.exceptions;
-
-import frc.robot.ShamLib.SMF.StateMachine;
-
-public class IncompatibleStateException extends Exception{
-    public IncompatibleStateException(StateMachine<?> machine, Object existing, Object newType) {
-        super("Incompatible state conversion in " + machine.getName() + " attempted. Details: \nExisting:" + existing + "\nNew:" + newType);
-    }
-}
