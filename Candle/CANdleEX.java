6693f711613165f337db3e2cee639792e7f0959e
Jacob Barta <jacob.barta25@gmail.com>
2023-02-03 13:52:39 -0500

    Create CANdleEX.java

---
+++ Candle/CANdleEX.java
@@ -0,0 +1,35 @@
+package frc.robot.ShamLib.Candle;
+
+import com.ctre.phoenix.led.*;
+
+public class CANdleEX extends CANdle {
+
+    private final int ledCount;
+
+    /**
+     * Constructs a new CANdleEX object
+     * @param canID id of the candle
+     * @param brightness brightness [0.0-1.0]
+     * @param ledCount number of leds in the string
+     */
+    public CANdleEX(int canID, double brightness, int ledCount) {
+        super(canID);
+
+        this.ledCount = ledCount;
+
+        CANdleConfiguration configAll = new CANdleConfiguration();
+        configAll.statusLedOffWhenActive = true;
+        configAll.disableWhenLOS = false;
+        configAll.stripType = LEDStripType.GRB;
+        configAll.brightnessScalar = brightness;
+        configAll.vBatOutputMode = VBatOutputMode.Modulated;
+        configAllSettings(configAll, 100);
+    }
+
+    public int getLedCount() {
+        return ledCount;
+    }
+
+    public void setBrightness(double percent) { configBrightnessScalar(percent, 0); }
+
+}
