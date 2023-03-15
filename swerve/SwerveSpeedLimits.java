5c0f640902ac3801de06a181bef98d6e9c1320b1
Jacob Barta <jacob.barta25@gmail.com>
2023-03-14 21:31:14 -0400

    wrote turbo mode

---
+++ swerve/SwerveSpeedLimits.java
@@ -0,0 +1,38 @@
+package frc.robot.ShamLib.swerve;
+
+public class SwerveSpeedLimits {
+    private double maxSpeed;
+    private double maxAcceleration;
+    private double maxRotationalSpeed;
+    private double maxRotationalAcceleration;
+
+    /**
+     * Represents the speed limits that a swerve drive can follow
+     * @param maxSpeed the maximum linear speed (m/s)
+     * @param maxAcceleration the maximum linear acceleration (m/s^2)
+     * @param maxRotationalSpeed the maximum rotational speed (rad/s)
+     * @param maxRotationalAcceleration the maximum rotational acceleration (rad/s^2)
+     */
+    public SwerveSpeedLimits(double maxSpeed, double maxAcceleration, double maxRotationalSpeed, double maxRotationalAcceleration) {
+        this.maxSpeed = maxSpeed;
+        this.maxAcceleration = maxAcceleration;
+        this.maxRotationalSpeed = maxRotationalSpeed;
+        this.maxRotationalAcceleration = maxRotationalAcceleration;
+    }
+
+    public double getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public double getMaxAcceleration() {
+        return maxAcceleration;
+    }
+
+    public double getMaxRotationalSpeed() {
+        return maxRotationalSpeed;
+    }
+
+    public double getMaxRotationalAcceleration() {
+        return maxRotationalAcceleration;
+    }
+}
