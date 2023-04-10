eb16351a209191eb592d447d196559ee5c275583
Jacob Barta <jacob.barta25@gmail.com>
2023-04-10 18:08:19 -0400

    Update AutonomousLoader.java

--- AutonomousLoader.java
+++ AutonomousLoader.java
@@ -1,17 +1,17 @@
 package frc.robot.ShamLib;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class AutonomousLoader<E extends Enum<E>> {
-    Map<E, Command> autoRoutes = new HashMap<>();
+public class AutonomousLoader<C extends CommandBase, E extends Enum<E>> {
+    Map<E, C> autoRoutes = new HashMap<>();
 
     private final SendableChooser chooser;
 
-    public AutonomousLoader(Map<E, Command> autoRoutes) {
+    public AutonomousLoader(Map<E, C> autoRoutes) {
         //Routes
         this.autoRoutes = autoRoutes;
 
@@ -22,7 +22,7 @@ public class AutonomousLoader<E extends Enum<E>> {
     private SendableChooser composeSendableChooser() {
         int iterations = 0;
         SendableChooser chooser = new SendableChooser();
-        for(Map.Entry<E, Command> e : autoRoutes.entrySet()) {
+        for(Map.Entry<E, C> e : autoRoutes.entrySet()) {
             if(iterations == 0) chooser.setDefaultOption(e.getKey().name(), e.getKey());
             else chooser.addOption(e.getKey().name(), e.getKey());
             iterations++;
@@ -35,7 +35,7 @@ public class AutonomousLoader<E extends Enum<E>> {
         return chooser;
     }
 
-    public Command getCurrentSelection() {
+    public C getCurrentSelection() {
         return autoRoutes.get(chooser.getSelected());
     }
 
