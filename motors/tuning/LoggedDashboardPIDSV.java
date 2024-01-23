5118fe022c19ec385269a95f22013d15923462dd
Breadspeed1 <53412269+Breadspeed1@users.noreply.github.com>
2024-01-23 15:37:53 -0500

    individual numbers

--- motors/tuning/LoggedDashboardPIDSV.java
+++ motors/tuning/LoggedDashboardPIDSV.java
@@ -2,26 +2,46 @@ package frc.robot.ShamLib.motors.tuning;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.ShamLib.motors.talonfx.PIDSVGains;
+import java.util.concurrent.atomic.AtomicBoolean;
 import org.littletonrobotics.junction.LogTable;
 import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.inputs.LoggableInputs;
 import org.littletonrobotics.junction.networktables.LoggedDashboardInput;
 
 public class LoggedDashboardPIDSV implements LoggedDashboardInput {
+  private static final String resetPath = "/reset";
+  private static final String valuesOrder = "PIDSV";
+
   private final String key;
   private PIDSVGains defaultValue;
   private PIDSVGains value;
+  private AtomicBoolean reset = new AtomicBoolean(false);
 
   private final LoggableInputs inputs =
       new LoggableInputs() {
         @Override
         public void toLog(LogTable table) {
-          table.put(key, value.toArray());
+          double[] numbers = value.toArray();
+
+          for (int i = 0; i < 5; i++) {
+            SmartDashboard.putNumber(key + "/" + valuesOrder.charAt(i), numbers[i]);
+          }
+
+          table.put(key + resetPath, reset.get());
         }
 
         @Override
         public void fromLog(LogTable table) {
-          value.set(table.get(key, defaultValue.toArray()));
+          double[] numbers = new double[5];
+          double[] defaults = defaultValue.toArray();
+
+          for (int i = 0; i < 5; i++) {
+            numbers[i] = SmartDashboard.getNumber(key + "/" + valuesOrder.charAt(i), defaults[i]);
+          }
+
+          value.set(numbers);
+
+          reset.set(table.get(key + resetPath, reset.get()));
         }
       };
 
@@ -30,11 +50,29 @@ public class LoggedDashboardPIDSV implements LoggedDashboardInput {
     this.defaultValue = defaultValue;
     this.value = defaultValue.clone();
 
-    SmartDashboard.putNumberArray(key, SmartDashboard.getNumberArray(key, defaultValue.toArray()));
+    putNumbers(getNumbers());
+    SmartDashboard.putBoolean(key + resetPath, reset.get());
 
     periodic();
 
     Logger.registerDashboardInput(this);
+  }
+
+  private void putNumbers(double[] numbers) {
+    for (int i = 0; i < 5; i++) {
+      SmartDashboard.putNumber(key + "/" + valuesOrder.charAt(i), numbers[i]);
+    }
+  }
+
+  private double[] getNumbers() {
+    double[] numbers = new double[5];
+    double[] defaults = defaultValue.toArray();
+
+    for (int i = 0; i < 5; i++) {
+      numbers[i] = SmartDashboard.getNumber(key + "/" + valuesOrder.charAt(i), defaults[i]);
+    }
+
+    return numbers;
   }
 
   public void setDefault(PIDSVGains defaultValue) {
@@ -46,7 +84,7 @@ public class LoggedDashboardPIDSV implements LoggedDashboardInput {
   }
 
   public void set(PIDSVGains value) {
-    SmartDashboard.putNumberArray(key, value.toArray());
+    putNumbers(value.toArray());
   }
 
   public PIDSVGains get() {
@@ -56,7 +94,15 @@ public class LoggedDashboardPIDSV implements LoggedDashboardInput {
   @Override
   public void periodic() {
     if (!Logger.hasReplaySource()) {
-      value.set(SmartDashboard.getNumberArray(key, defaultValue.toArray()));
+      if (reset.get()) {
+        set(defaultValue);
+        reset.set(false);
+
+        SmartDashboard.putBoolean(key + resetPath, reset.get());
+      }
+
+      reset.set(SmartDashboard.getBoolean(key + resetPath, reset.get()));
+      value.set(getNumbers());
     }
     Logger.processInputs(prefix, inputs);
   }
