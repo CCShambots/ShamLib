4d94984df651297cba6b7ff232896c0771871a21
Jacob Barta <jacob.barta25@gmail.com>
2024-02-01 13:08:38 -0500

    spotless

--- WhileDisabledInstantCommand.java
+++ WhileDisabledInstantCommand.java
@@ -4,14 +4,12 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 public class WhileDisabledInstantCommand extends InstantCommand {
 
-    public WhileDisabledInstantCommand(Runnable toRun) {
-        super(toRun);
-    }
+  public WhileDisabledInstantCommand(Runnable toRun) {
+    super(toRun);
+  }
 
-    @Override
-    public boolean runsWhenDisabled() {
-        return true;
-    }
-
-
+  @Override
+  public boolean runsWhenDisabled() {
+    return true;
+  }
 }
