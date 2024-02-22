96076daa8eb40f802d5efefa16e2620efd16a0e0
Jacob Barta <jacob.barta25@gmail.com>
2024-02-22 08:12:18 -0500

    spotless

--- motors/talonfx/EnhancedTalonFX.java
+++ motors/talonfx/EnhancedTalonFX.java
@@ -7,13 +7,6 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.FunctionalCommand;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.BooleanSupplier;
 
 public class EnhancedTalonFX extends TalonFX {
   private final double inputToOutputRatio;
